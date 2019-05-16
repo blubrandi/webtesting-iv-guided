77a7de4abe31ecf340d820428e2e117fe656fd21
blubrandi <blubrandi0@gmail.com>
2019-05-16 12:29:42 -0600

    Added comment for command to run to migrate latest on heroku app

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -5,4 +5,9 @@ const dbEnv = process.env.DB_ENV || 'development';
 //add Config Vars on Heroku --- 
 //DB_ENV production
 
+//Run npx heroku run knex:latest -a testing-guided
+//to migrate latest on heroku app
+
+
+
 module.exports = knex(config[dbEnv]);
