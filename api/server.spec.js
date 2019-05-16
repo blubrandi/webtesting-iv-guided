93e18cf3addf76adc2d0546f289895c70a280d84
blubrandi <blubrandi0@gmail.com>
2019-05-16 11:20:28 -0600

    Added server test

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,7 +1,19 @@
+//supertest
 
+const request = require('supertest')
+
+const server = require('./server.js')
 
 describe('server', () => {
+
     it('sets the environment to testing', () => {
         expect(process.env.DB_ENV).toBe('testing')
+    })
+
+    //open client, make a request and inspect the response
+    describe('GET /', () => {
+        it('should return 200 OK', () => {
+            request(server).get('/').expect(500)
+        })
     })
 })
