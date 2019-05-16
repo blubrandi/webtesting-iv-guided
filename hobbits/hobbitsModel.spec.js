1ec131861dc0c4055d604efa261d10d98b1945c0
blubrandi <blubrandi0@gmail.com>
2019-05-16 11:57:38 -0600

    Added beforeAll to clean the db before testing again

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -2,6 +2,11 @@ const db = require('../data/dbConfig.js')
 const Hobbits = require('./hobbitsModel.js')
 
 describe('Hobbits Model', () => {
+
+    beforeAll(async () => {
+        await db('hobbits').truncate()
+    })
+
     describe('insert()', () => {
         it('should insert provided hobbit', async () => {
             await Hobbits.insert({ name: 'gaffer' })
