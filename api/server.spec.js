dec9a78e0b2457d15eb422d1641e51ac1e735750
blubrandi <blubrandi0@gmail.com>
2019-05-16 11:35:52 -0600

    Added 3 total ways to test async

--- api/server.spec.js
+++ api/server.spec.js
@@ -13,8 +13,27 @@ describe('server', () => {
     //open client, make a request and inspect the response
     describe('GET /', () => {
         it('should return 200 OK', () => {
-            return request(server).get('/').expect(200)
-            //add return to turn on async testing
-        })
-    })
-})
+            // we return the promise
+            return request(server)
+                .get('/')
+                .expect(200);
+        });
+
+        it('using the squad (async/await)', async () => {
+            // use the squad
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+
+        it('should return JSON using done callback', done => {
+            // using the done callback
+            request(server)
+                .get('/')
+                .then(res => {
+                    expect(res.type).toBe('application/json'); // Content-Type
+                    done();
+                });
+        });
+
+    });
+});
